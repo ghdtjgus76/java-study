ba70a18ae1476f8d65d36239234618666bfafc71
ghdtjgus76 <ghdtjgus76@naver.com>
2024-12-05 13:13:39 +0900

    fix: NullPointerException 오류 해결

--- src/access/ex/ShoppingCart.java
+++ src/access/ex/ShoppingCart.java
@@ -17,7 +17,8 @@ public class ShoppingCart {
     public void displayItems() {
         System.out.println("장바구니 상품 출력");
 
-        for (Item item : items) {
+        for (int i = 0; i < itemCount; i++) {
+            Item item = items[i];
             System.out.println("상품명:" + item.getName() + ", 합계:" + item.getTotalPrice());
         }
 
@@ -27,7 +28,8 @@ public class ShoppingCart {
     private int calculateTotalPrice() {
         int totalPrice = 0;
 
-        for (Item item : items) {
+        for (int i = 0; i < itemCount; i++) {
+            Item item = items[i];
             totalPrice += item.getTotalPrice();
         }
 
